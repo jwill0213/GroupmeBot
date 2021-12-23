880f80abb94613d488151533bd6b571af08cdd2c
Jordan Williams <jtw0213@gmail.com>
2021-12-23 15:56:22 -0600

    Add example .env to show all env variables needed

--- scheduleScripts/sheetsTest.py
+++ scheduleScripts/sheetsTest.py
@@ -2,7 +2,6 @@ import gspread
 
 from Utils.redisUtils import getRedisClient
 
-SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
 SERVICE_ACCOUNT_FILE = 'key.json'
 
 # The ID and range of a sample spreadsheet.
@@ -14,21 +13,22 @@ def main():
 
     # Call the Sheets API
     spreadsheet = gc.open_by_key(SAMPLE_SPREADSHEET_ID)
-    sheetAPI = service.spreadsheets()
-    result = sheetAPI.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
-                                   range="TIME!A1:B30").execute()
-    values = result.get('values', [])
 
-    req = sheetAPI.values().update(spreadsheetId=SAMPLE_SPREADSHEET_ID,
-                                   valueInputOption='USER_ENTERED', range='SHIB!A1:B30', body={'values': values}).execute()
+    # sheetAPI = service.spreadsheets()
+    # result = sheetAPI.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
+    #                                range="TIME!A1:B30").execute()
+    # values = result.get('values', [])
 
-    if not values:
-        print('No data found.')
-    else:
-        print('Name, Major:')
-        for row in values:
-            # Print columns A and E, which correspond to indices 0 and 4.
-            print('%s, %s' % (row[0], row[1]))
+    # req = sheetAPI.values().update(spreadsheetId=SAMPLE_SPREADSHEET_ID,
+    #    valueInputOption='USER_ENTERED', range='SHIB!A1:B30', body={'values': values}).execute()
+
+    # if not values:
+    #     print('No data found.')
+    # else:
+    #     print('Name, Major:')
+    #     for row in values:
+    #         # Print columns A and E, which correspond to indices 0 and 4.
+    #         print('%s, %s' % (row[0], row[1]))
 
 
 if __name__ == '__main__':
